892614cf28ffc12faff1f92607755397c9b84a9e
Amangoua Ange <adagou@yahoo.com>
2024-01-23 20:06:41 +0000

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import "./index.css";
-
+import bg from "./assets/background.mp4";
 function App() {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <main className="h-full w-full">
+      <div className="h-screen w-full relative">
+        <video
+          autoPlay
+          loop
+          muted
+          src={bg}
+          className="h-full w-full object-cover absolute"
+        ></video>
+        <header className="h-full w-full relative flex z-50 flex-col justify-between items-center">
+          <nav className="text-white wrapper text-3xl flex items-center justify-between w-full p-5">
+            <h1>Aside agency.</h1>
+            <button>Menu</button>
+          </nav>
+          <h1 className="font-re font-thin rej text-white">Rejouice</h1>
+        </header>
+      </div>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        h1: "32rem",
+      },
+      fontFamily: {
+        nb: "nb",
+        re: "re",
+      },
+    },
   },
   plugins: [],
-}
+};
